91f289b9ba99328ef402dbc16dbd36bae18497d6
Emma <151248139+CuzImStupi4@users.noreply.github.com>
2025-01-31 22:32:02 +0100

    Also fixed it in the About us page

--- about/cursor.js
+++ about/cursor.js
@@ -61,3 +61,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+document.addEventListener(
+  "wheel",
+  function (e) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && (e.key === "+" || e.key === "-" || e.key === "=")) {
+    e.preventDefault();
+  }
+});
